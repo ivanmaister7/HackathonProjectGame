3e060842fff0bbfbb033c22ddfde9245dcbd3847
ivanmaister7 <ivanmaister7@gmail.com>
2024-09-21 21:09:47 +0300

    menu and first level added

---
+++ HackathonProjectGame/Menu/MenuCell.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCell.swift
+//  HackathonProjectGame
+//
+//  Created by ivan on 21.09.2024.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
